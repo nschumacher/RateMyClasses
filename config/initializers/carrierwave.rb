8a0e881b5d5964c753d98da332ac84feeadf94ae
Nick Schumacher <nschuma@purdue.edu>
2017-01-29 02:22:58 -0500

    uncommented carrierwave.rb to work

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
-  # config.fog_credentials = {
-  #   :provider               => 'AWS',                          # required
-  #   :aws_access_key_id      => ENV["aws_access_key_id"],       # required
-  #   :aws_secret_access_key  => ENV["aws_secret_access_key"],   # required
-  # }
-  # config.fog_directory  = ENV['fog_directory']       # required
-  # config.fog_public     = false                      # optional, defaults to true
-  # config.fog_authenticated_url_expiration = 600      # (in seconds) => 10 minutes
+  config.fog_credentials = {
+    :provider               => 'AWS',                          # required
+    :aws_access_key_id      => ENV["aws_access_key_id"],       # required
+    :aws_secret_access_key  => ENV["aws_secret_access_key"],   # required
+  }
+  config.fog_directory  = ENV['fog_directory']       # required
+  config.fog_public     = false                      # optional, defaults to true
+  config.fog_authenticated_url_expiration = 600      # (in seconds) => 10 minutes
 end
